472b4577774a1c12e1d6c8f59196fa640a5af6e8
Janssens Luc <luc.janssens.lj@gmail.com>
2016-11-04 16:13:20 +0100

    First code trail

---
+++ src/main/java/com/brownfield/Product.java
@@ -0,0 +1,50 @@
+package com.brownfield;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the product database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Product.findAll", query="SELECT p FROM Product p")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+
+	private String description;
+
+	private String name;
+
+	public Product() {
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
